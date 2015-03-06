1a67125b2115fe50fd1bf26c1cd7ceed4ab1323f
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-06 17:12:39 +0000

    imported project from eclipse (sort of)

---
+++ test/CircleTests.js
@@ -0,0 +1,9 @@
+/* global circle */
+//var circle = require
+TestCase("CircleTest",{
+    "test should implicitly bind to object": function (){
+        assertEquals(12,circle.diameter());
+        
+    }
+    
+});
